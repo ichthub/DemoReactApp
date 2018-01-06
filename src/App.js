407a4eddf1a82e8b65d2f4b890bbedc8cb56bd98
ichthub <hassanhoumaid92@gmial.com>
2018-01-06 01:48:02 +0000

    Some webpack config and CSS Modules : pseudo Selectors and media Queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -45,18 +45,10 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-	  color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
 	  
-    };
-
-    let persons = null;
-
+	let persons = null;
+	let btnClasses =' ';
+	  
     if ( this.state.showPersons ) {
       persons = (
         <div>
@@ -70,25 +62,24 @@ class App extends Component {
           })}
         </div>
       );  
-		style.backgroundColor='red';
-		
-    }
+		btnClasses = classes.Red;
+}
 
-	let classes =[];
+	let assignedClasses =[];
 	if (this.state.persons.length <= 2){
-		classes.push('red');
+		assignedClasses.push(classes.red);
 	}
 	if (this.state.persons.length <= 1){
-		classes.push('bold');
+		assignedClasses.push(classes.bold);
 	}
 	
     return (
 	
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className = {classes.join(' ')}>This is really working!</p>
+        <p className = {assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className = {btnClasses}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
