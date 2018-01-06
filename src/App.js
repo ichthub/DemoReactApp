b2bd572e5d5779c4bc9dd92c45bb4278048c4796
ichthub <hassanhoumaid92@gmial.com>
2018-01-06 00:13:04 +0000

    Intro to CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -52,12 +51,8 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-	  ':hover':{
-		  backgroundColor:'lightgreen',
-		  color:'black',
-		  
-	  }
+      cursor: 'pointer'
+	  
     };
 
     let persons = null;
@@ -76,11 +71,7 @@ class App extends Component {
         </div>
       );  
 		style.backgroundColor='red';
-		style[':hover']={
-		  backgroundColor:'salmon',
-		  color:'black',
-		  
-	  }
+		
     }
 
 	let classes =[];
@@ -92,8 +83,7 @@ class App extends Component {
 	}
 	
     return (
-	<StyleRoot>
-		
+	
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className = {classes.join(' ')}>This is really working!</p>
@@ -102,11 +92,9 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
-	  
-	</StyleRoot>
-    );
+	);
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
