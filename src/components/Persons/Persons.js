44939585953fd220e95ef05953c9825b6e69f706
ichthub <hassanhoumaid92@gmial.com>
2018-01-10 01:59:04 +0000

    components creation lifecycle (trigger update)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,7 +13,24 @@ class Persons extends Component{
 	componentDidMount () {
 		console.log('[Persons.js] inside ComponentDidMount()');
 	}
+	//Lifecycle: trigger update by parent
 	
+	componentWillReceiveProps ( nextProps ){
+		console.log("[UPDATE Persons.js] Inside componentWillReceiveProps", nextProps);
+	}
+	
+	shouldComponentUpdate ( nextProps, nextState ){
+		console.log("[UPDATE Persons.js] Inside shouldComponentUpdate", nextProps, nextState);
+		return nextProps.persons !== this.props.persons ;
+	}
+	
+	componentWillUpdate ( nextProps, nextState ){
+		console.log("[UPDATE Persons.js] Inside componentWillUpdate", nextProps, nextState);
+	}
+	
+	componentDidUpdate (){
+		console.log("[UPDATE Persons.js] Inside componentDidUpdate");
+	}
 	
 	
 	render(){
