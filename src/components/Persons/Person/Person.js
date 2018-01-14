bb5ba0d8f3c92c987dceb051f4eb5de79bb9c35e
ichthub <hassanhoumaid92@gmial.com>
2018-01-14 02:32:36 +0000

    first approach to HOCs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import WithClass from '../../../HOC/WithClass';
 
 class Person extends Component{
 	
@@ -21,19 +22,18 @@ class Person extends Component{
 		console.log('[Person.js] inside render ()');
 		
 		return (
-        <div className={classes.Person}>
+        <WithClass classes={classes.Person}>
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name} />
-        </div>
+        </WithClass>
     )
 	}
 }
 
-
+//changing Person.js from stateless ot stateful component
 /*
 const person = ( props ) => {
-	
 	
     return (
         <div className={classes.Person}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../HOC/WithClass';
+
 
 class App extends PureComponent {
 	constructor (props) {
@@ -90,7 +92,7 @@ class App extends PureComponent {
 	
     return (
 	
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
 		<button onClick = {()=>{this.setState({showPersons: true})}}>Show Button</button>
 		<Cockpit
 			appTitle = {this.props.title}
@@ -99,7 +101,7 @@ class App extends PureComponent {
 			clicked = {this.togglePersonsHandler}
 		/>
         {persons}
-      </div>
+      </WithClass>
 	);
 	}
 }
